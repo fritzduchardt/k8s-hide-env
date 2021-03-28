8b9c4ec9139332c0a41a42b42f40eb42f2756d42
fritz duchardt <fritz@duchardt.net>
2021-03-28 09:52:58 +0200

    Complete location reshuffling

---
+++ cmd/k8s-hide-env/main_test.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"io/ioutil"
+	"testing"
+)
+
+func TestCreateAdmissionResponse(t *testing.T) {
+	json, err := ioutil.ReadFile("../../test/admission-request.yaml")
+	if err != nil {
+		t.Errorf("Can't open file: %v", err)
+	}
+	response, err := createAdmissionResponse(string(json))
+	if err != nil {
+		t.Errorf("%+v", err)
+		return
+	}
+	if response == "" {
+		t.Errorf("Invalid response %v", response)
+		return
+	}
+}
--- src/main/go/k8shideenv/go.mod
+++
@@ -1,8 +0,0 @@
-module github.com/fritzduchardt/k8shideenv
-
-go 1.16
-
-require (
-	github.com/pkg/errors v0.9.1 // indirect
-	gopkg.in/yaml.v2 v2.4.0
-)
